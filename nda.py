f83bcea187ccc3b96340b357245605b0ca178d4e
Marissa Ramirez Zweiger <mzweig@berkeley.edu>
2017-10-04 17:24:57 -0700

    Set up scattering source.

--- nda.py
+++ nda.py
@@ -81,9 +81,9 @@ class NDA(object):
         rhs for all components
         Generate numpy arrays and put them in self.
         '''
-        f = sps.lil_matrix((nodes, 1)) # just use numpy array
-        q = sps.lil_matrix((nodes, 1))
-        j = sps.lil_matrix((nodes, 1))
+        f = np.array(nodes)
+        q = np.array(nodes)
+        j = np.array(nodes)
 
         elem_fs = [] # list of elementary vector based on material
         elem_qs = []
@@ -147,7 +147,6 @@ class NDA(object):
                     xx += 1
 
             fixed_rhs = f + q + j
-            fixed_rhs = sps.csc_matrix(fixed_rhs)
             fixed_rhses[g] = fixed_rhs
 
     def assemble_group_bilinear_forms(self,g):
@@ -212,7 +211,6 @@ class NDA(object):
             cell_i = cell//int(np.sqrt(n))
             cell_j = cell%int(np.sqrt(n))
 
-
             mat_id = mat_map.get('id', n) # retrieve material id
             m = materials.index(mat_id)   # find material index
 
@@ -244,8 +242,7 @@ class NDA(object):
                 yy += 1
             xx += 1
 
-        lhs = A + B + C
-        return sps.csc_matrix(lhs) # don't return
+        self.sys_mats = sps.csc_matrix(A + B + C)
 
     def assemble_group_linear_forms(self,g):
         '''@brief Function used to assemble linear forms for Group g
@@ -253,6 +250,44 @@ class NDA(object):
         assert 0<=g<=self.mat.n_group, 'Group index out of range'
         # TODO: fill in linear form assembly code
         # Scattering + fixed source 
+        
+        materials = self.mat.ids()
+        M = el.mass
+        
+        s = np.array(nodes)
+        elem_alls = [] # list of elementary vector based on material
+    
+        area = h**2 # retrieve h
+
+        # Set up elementary matrices for each material and group 
+        for g in xrange(n_grp):
+            for m in materials:
+            
+                # Retrieve cross-sections for cell
+                sig_s = self.mat.get('sig_s', mat_id=m)[g]
+                elem_s = sig_s*M
+                elem_alls.append(elem_s)
+
+
+            for cell in xrange(n):
+                cell_i = cell//int(np.sqrt(n))
+                cell_j = cell%int(np.sqrt(n))
+
+                mat_id = mat_map.get('id', n) # retrieve material id
+                m = materials.index(mat_id)
+                # Global to Local node mapping
+                """Nodes are flattened by row with row x=0 going first.
+                That means node x0,y0 in cell0 has a global index of 0
+                and node x1,y1 in cell0 has a global index of nodes_x+1"""
+                mapping = np.array([cell+cell_i, cell+cell_i+1, 
+                            cell+cell_i+n_x+1, cell+cell_i+n_x+2])
+
+                xx = 0
+                for ii in mapping:
+                    s[ii, 0] += elem_alls[m, xx]
+                    xx += 1
+
+            self.sys_rhses[g] = s + self.fixed_rhses[g]      
 
     def assemble_bilinear_forms(self):
         for g in xrange(self.n_grp):
