e429900b162480aaa7257708fb5488700b683f06
Marissa Ramirez Zweiger <mzweig@berkeley.edu>
2017-10-13 21:18:08 -0700

    Ran yapf (pep8 formatting tool) on run.py

--- run.py
+++ run.py
@@ -14,23 +14,27 @@ from input_kaist_mox_1 import problem
 # Build Material Library
 
 try:
-    MAT_LIB = material.mat_lib(n_grps = problem['groups'],
-                             files = problem['materials'],
-                             tr_scatt = problem['tr_scatt'])
+    MAT_LIB = material.mat_lib(
+        n_grps=problem['groups'],
+        files=problem['materials'],
+        tr_scatt=problem['tr_scatt'])
 except KeyError:
-    MAT_LIB = material.mat_lib(n_grps = problem['groups'],
-                             files = problem['materials'])
+    MAT_LIB = material.mat_lib(
+        n_grps=problem['groups'], files=problem['materials'])
 
 # Build Material Mapping
 
-MAT_MAP = material.mat_map(lib = MAT_LIB, layout = problem['layout'],
-                           layout_dict = problem['layout_dict'],
-                           x_max=problem['domain_upper'],
-                           n=problem['mesh_cells'])
+MAT_MAP = material.mat_map(
+    lib=MAT_LIB,
+    layout=problem['layout'],
+    layout_dict=problem['layout_dict'],
+    x_max=problem['domain_upper'],
+    n=problem['mesh_cells'])
 
 # Build Mesh
 
 MESH = mesh.Mesh(problem['mesh_cells'], problem['domain_upper'], MAT_MAP)
+
 
 def run():
     # do we do NDA
@@ -48,5 +52,6 @@ def run():
         # eigen solving
         eigen_cls.do_iterations(ho_cls=ho_cls, nda_cls=nda_cls)
     # TODO: output and plotting functionality
+
 
 run()
